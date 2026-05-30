938c5bf9b76d341ccd9612963758e32a399ba66d
momo <284926839+Mice999@users.noreply.github.com>
2026-05-30 20:47:22 +0800

    ci: build macOS Universal Binary (.app) supporting both arm64 and x86_64

--- build.py
+++ build.py
@@ -3,14 +3,20 @@
 Uses os.pathsep so --add-data works on both Unix (':') and Windows (';')
 without shell-level escaping differences.
 """
+import argparse
 import os
 import sys
 
 import PyInstaller.__main__
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--arch', default=None)
+parser.add_argument('--universal', action='store_true')
+args, _ = parser.parse_known_args()
+
 SEP = os.pathsep
 
-args = [
+pyinstaller_args = [
     "--onefile",
     "--name", "fic-guard",
     "--collect-all", "rich",
@@ -23,7 +29,14 @@ args = [
     "fic_guard_entry.py",
 ]
 
-if sys.platform in ("win32", "darwin"):
-    args.append("--windowed")
+if sys.platform == "win32":
+    pyinstaller_args.append("--windowed")
+elif sys.platform == "darwin" and args.universal:
+    pyinstaller_args.append("--windowed")
 
-PyInstaller.__main__.run(args)
+if args.arch:
+    pyinstaller_args.append(f'--target-arch={args.arch}')
+if args.universal:
+    pyinstaller_args.append('--target-arch=universal2')
+
+PyInstaller.__main__.run(pyinstaller_args)
